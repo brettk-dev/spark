37d2b20efd73d61b403f53fb5314110cc8e9552d
Brett Kinnamon <>
2021-05-11 13:55:24 -0400

    add the directory option

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,17 @@ fn main() {
     .about("Generates components with tests.")
     .arg(Arg::with_name("framework"))
     .arg(Arg::with_name("name"))
+    .arg(Arg::with_name("dir"))
     .get_matches();
 
   let framework = matches.value_of("framework").unwrap_or("vue");
   let framework = match framework {
     "vue" => Some(generator::Framework::Vue),
+    "react" => Some(generator::Framework::React),
+    "svelte" => Some(generator::Framework::Svelte),
     _ => None,
   };
   let name = matches.value_of("name").unwrap_or("HelloWorld");
-  generator::generate_component(
-    framework.unwrap_or(generator::Framework::Vue),
-    name,
-    Some("./output"),
-  );
+  let dir = matches.value_of("dir");
+  generator::generate_component(framework.unwrap_or(generator::Framework::Vue), name, dir);
 }
